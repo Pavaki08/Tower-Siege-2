e232ab512323190a3aa27e942d8cacd648b5d662
Pavaki08 <73833526+Pavaki08@users.noreply.github.com>
2020-12-19 19:56:23 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,33 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB ;
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+
+    fly() {
+        this.slingshot.bodyA = null ;
+    }
+
+    attach(body){
+        this.slingshot.bodyA = body ;
+    }
+
+    display(){
+        push()
+        if (this.slingshot.bodyA){
+        var pointA = this.slingshot.bodyA.position;
+        var pointB = this.pointB ;
+        strokeWeight(4);
+        stroke("cyan")
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    pop()
+}
+}
